8dd92684033a24c26637cb7b027359d9b5043870
ThanhTien3103 <thanhtien31032002@gmail.com>
2022-08-05 16:36:29 +0700

    change mail

--- mail.php
+++ mail.php
@@ -8,31 +8,12 @@
             echo "Invalid email format";
         }
         else{
-            $nFrom = "ThanhDuongShop";    //mail duoc gui tu dau, thuong de ten cong ty ban
-            $mFrom = 'testuser12921@gmail.com';  //dia chi email cua ban 
-            $mPass = '@test123';       //mat khau email cua ban
-            $mTo = $email_i;   //dia chi nhan mail
-            $nTo = "KhachHang ".$email_i;
-            $mail = new PHPMailer();
-            $body             = 'Có Phải Bạn đang cần đặt dịch vụ của chúng tôi';   // Noi dung email
-            $title = 'Dịch Vụ ThanhDuongShop';   //Tieu de gui mail
-            $mail->IsSMTP();             
-            $mail->CharSet  = "utf-8";
-            $mail->SMTPDebug  = 0;   // enables SMTP debug information (for testing)
-            $mail->SMTPAuth   = true;    // enable SMTP authentication
-            $mail->SMTPSecure = "ssl";   // sets the prefix to the servier
-            $mail->Host       = "smtp.gmail.com";    // sever gui mail.
-            $mail->Port       = 465;         // cong gui mail
-            // xong phan cau hinh bat dau phan gui mail
-            $mail->Username   = $mFrom;  // khai bao dia chi email
-            $mail->Password   = $mPass;              // khai bao mat khau
-            $mail->SetFrom($mFrom, $nFrom);
-            $mail->AddReplyTo('testuser12921@gmail.com', 'ThanhDuongShop'); //khi nguoi dung phan hoi se duoc gui den email nay
-            $mail->Subject    = $title;// tieu de email 
-            $mail->MsgHTML($body);// noi dung chinh cua mail se nam o day.
-            $mail->AddAddress($mTo, $nTo);
-            // thuc thi lenh gui mail 
-            if(!$mail->Send()) {
+            $to = $email_i;
+            $subject = "Dịch Vụ ThanhDuongShop";
+            $messeger = "Có Phải Bạn đang cần đặt dịch vụ của chúng tôi";
+            $header = "From:testuser12921@gmail.com \r\n";
+            $success = mail($to,$subject,$messeger,$header);
+            if($success == false) {
                 echo 'Có lỗi xảy ra!';
                  
             } else {
